df1268fd0fb364e1647e23d3e2e8cee63cdc71b4
Soojungkim <soojung.kimm@gmail.com>
2024-01-29 17:34:32 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'tim';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'murloc';
+}
+
+changeLeastFavoriteCustomer();
